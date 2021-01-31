347482cf3a5e1ed307f3f5588633f1cd4737db7e
Kim Kim <52686208+selenalee123@users.noreply.github.com>
2021-01-30 20:27:39 -0500

    completed api 2

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
-import React, { useEffect } from 'react';
-import { StyleSheet, Text, SafeAreaView, View } from 'react-native';
-import Home from './src/screens/Home';
-import Post from './src/components/Post';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
 import 'react-native-gesture-handler';
-import RootNavigation from './src/navigation';
-import HomeBottomTabNavigator from './src/navigation';
+import React, {useEffect} from 'react';
+import {SafeAreaView, StatusBar} from 'react-native';
+
+import Navigation from './src/navigation';
+
+import {Auth, API, graphqlOperation} from 'aws-amplify';
+import {withAuthenticator} from 'aws-amplify-react-native';
+
+import {createUser} from './src/graphql/mutations';
+import {getUser} from './src/graphql/queries';
+
 import Amplify from 'aws-amplify'
 import config from './aws-exports'
 Amplify.configure(config)
-
-
-import { Auth, API, graphqlOperation } from 'aws-amplify';
-import { withAuthenticator } from 'aws-amplify-react-native';
-
-import { createUser } from './src/graphql/mutations';
-import { getUser } from './src/graphql/queries';
 
 const randomImages = [
   'https://hieumobile.com/wp-content/uploads/avatar-among-us-2.jpg',
@@ -27,9 +33,7 @@ const getRandomImage = () => {
   return randomImages[Math.floor(Math.random() * randomImages.length)];
 };
 
-
-const App = () => {
-
+const App: () => React$Node = () => {
   useEffect(() => {
     const fetchUser = async () => {
       // get currently authenticated user
@@ -63,18 +67,14 @@ const App = () => {
     fetchUser();
   }, []);
 
-
   return (
-    <SafeAreaView style={styles.container}>
-      <HomeBottomTabNavigator />
-    </SafeAreaView>
+    <>
+      <StatusBar barStyle="light-content" />
+      <SafeAreaView style={{flex: 1, backgroundColor: 'black'}}>
+        <Navigation />
+      </SafeAreaView>
+    </>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
 
 export default withAuthenticator(App);
--- data/posts.js
+++ data/posts.js
@@ -1,31 +1,31 @@
 export default [
   {
     id: 'p1',
-    videoUri: require('../src/assets/videos/demo.mp4'),
+    videoUri: 'https://d8vywknz0hvjw.cloudfront.net/fitenium-media-prod/videos/45fee890-a74f-11ea-8725-311975ea9616/proccessed_720.mp4',
     user: {
       id: 'u1',
-      username: '@kimkim',
-      imageUri: require('../src/assets/images/kimkim.png'),
+      username: 'daviddobrik',
+      imageUri: 'https://influencermarketinghub.com/wiki/wp-content/uploads/2020/08/1657369265111046_c5_720x720.jpeg',
     },
-    description: 'Sugar Shack ',
-    songName:  'NF- the search',
-    songImage: '',
+    description: 'hahahah oh boy @borat',
+    songName: 'NF - The search',
+    songImage: 'https://influencermarketinghub.com/wiki/wp-content/uploads/2020/08/1657369265111046_c5_720x720.jpeg',
     likes: 123,
-    comments: 144,
+    comments: 12,
     shares: 44,
   },
+
   {
     id: 'p2',
-    videoUri: require('../src/assets/videos/darin.mp4'),
+    videoUri: 'https://d8vywknz0hvjw.cloudfront.net/fitenium-media-prod/videos/45fee890-a74f-11ea-8725-311975ea9616/proccessed_720.mp4',
     user: {
-      id: 'u2',
-      username: '@kimkim',
-      imageUri: require('../src/assets/images/kimkim.png'),
+      id: 'u1',
+      username: 'Test',
+      imageUri: 'https://influencermarketinghub.com/wiki/wp-content/uploads/2020/08/1657369265111046_c5_720x720.jpeg',
     },
-    description: 'never give up ',
+    description: 'hahahah oh boy @borat',
     songName: 'NF - The search',
-    songImage:
-      'https://influencermarketinghub.com/wiki/wp-content/uploads/2020/08/1657369265111046_c5_720x720.jpeg',
+    songImage: 'https://influencermarketinghub.com/wiki/wp-content/uploads/2020/08/1657369265111046_c5_720x720.jpeg',
     likes: 123,
     comments: 12,
     shares: 44,
@@ -33,24 +33,20 @@ export default [
 
   {
     id: 'p3',
-    videoUri: require('../src/assets/videos/montreal.mp4'),
+    videoUri: 'https://d8vywknz0hvjw.cloudfront.net/fitenium-media-prod/videos/45fee890-a74f-11ea-8725-311975ea9616/proccessed_720.mp4',
     user: {
-      id: 'u2',
-      username: 'selena',
-      imageUri: require('../src/assets/images/kimkim.png'),
+      id: 'u1',
+      username: 'User 3',
+      imageUri: 'https://influencermarketinghub.com/wiki/wp-content/uploads/2020/08/1657369265111046_c5_720x720.jpeg',
     },
     description: 'hahahah oh boy @borat',
     songName: 'NF - The search',
-    songImage:
-      'https://influencermarketinghub.com/wiki/wp-content/uploads/2020/08/1657369265111046_c5_720x720.jpeg',
+    songImage: 'https://influencermarketinghub.com/wiki/wp-content/uploads/2020/08/1657369265111046_c5_720x720.jpeg',
     likes: 123,
     comments: 12,
     shares: 44,
   },
-];
-
-
-
+]
 
 
 // export default [
